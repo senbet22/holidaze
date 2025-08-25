5cbb4ba646a7a9a4ed1644155dc2173f8a16dbee
senbet22 <senbet22@users.noreply.github.com>
2025-08-25 11:51:47 +0200

    update to assets

--- src/assets/assets.mjs
+++ src/assets/assets.mjs
@@ -1,25 +1,41 @@
-import cross_icon from "./cross_icon.svg";
-import menu_icon from "./menu_icon.svg";
-import check_icon from "./check_icon.svg";
+// Logo
 import dunestay_logo from "./dunestay_light.svg";
 import dunestay_dark_logo from "./dunestay_dark.svg";
-import star_icon from "./star_icon.svg";
-import location_icon from "./location_icon.svg";
 import dunestay_logo_text from "./dunestay_logo_text.svg";
 import dunestay_logo_text_dark from "./dunestay_logo_text_dark.svg";
+
+// Icons
+import cross_icon from "./cross_icon.svg";
+import parking_icon from "./parking_icon.svg";
+import search_icon from "./search_icon.svg";
+import search_icon_dark from "./search_icon_dark.svg";
+import pets_icon from "./pets_icon.svg";
+import wifi_icon from "./wifi_icon.svg";
+import breakfast_icon from "./breakfast_icon.svg";
+import menu_icon from "./menu_icon.svg";
+import check_icon from "./check_icon.svg";
+import star_icon from "./star_icon.svg";
+import location_icon from "./location_icon.svg";
 import dropdown_icon from "./dropdown_icon.svg";
 import desert_background from "./desert_background.png";
 import desert_home_bg from "./desert_home.avif";
+
 export const assets = {
+  dunestay_logo,
+  dunestay_dark_logo,
+  dunestay_logo_text,
+  dunestay_logo_text_dark,
+  parking_icon,
+  search_icon,
+  search_icon_dark,
+  pets_icon,
+  wifi_icon,
+  breakfast_icon,
   cross_icon,
   check_icon,
   menu_icon,
-  dunestay_logo,
-  dunestay_dark_logo,
   star_icon,
   location_icon,
-  dunestay_logo_text,
-  dunestay_logo_text_dark,
   dropdown_icon,
   desert_background,
   desert_home_bg,
