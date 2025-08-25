355abbe6c393087968f843205df1da68a98ee9f8
senbet22 <senbet22@users.noreply.github.com>
2025-08-25 11:44:50 +0200

    Added assets/icons in the assets folder

---
+++ src/assets/assets.mjs
@@ -0,0 +1,26 @@
+import cross_icon from "./cross_icon.svg";
+import menu_icon from "./menu_icon.svg";
+import check_icon from "./check_icon.svg";
+import dunestay_logo from "./dunestay_light.svg";
+import dunestay_dark_logo from "./dunestay_dark.svg";
+import star_icon from "./star_icon.svg";
+import location_icon from "./location_icon.svg";
+import dunestay_logo_text from "./dunestay_logo_text.svg";
+import dunestay_logo_text_dark from "./dunestay_logo_text_dark.svg";
+import dropdown_icon from "./dropdown_icon.svg";
+import desert_background from "./desert_background.png";
+import desert_home_bg from "./desert_home.avif";
+export const assets = {
+  cross_icon,
+  check_icon,
+  menu_icon,
+  dunestay_logo,
+  dunestay_dark_logo,
+  star_icon,
+  location_icon,
+  dunestay_logo_text,
+  dunestay_logo_text_dark,
+  dropdown_icon,
+  desert_background,
+  desert_home_bg,
+};
